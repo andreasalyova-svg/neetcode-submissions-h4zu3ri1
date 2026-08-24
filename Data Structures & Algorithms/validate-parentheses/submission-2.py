e53debe06353b8c5358c872892f0cc4327337356
andreasalyova-svg <andreasalyova@gmail.com>
2026-08-24 12:08:05 +0200

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) % 2 != 0:
+            return False
+        else:
+            b = []
+            chars = {']':'[', '}':'{', ')':'('}
+            stack = []
+            for char in s:
+                if char in chars:
+                    top_element = stack.pop() if stack else "#"
+                    if top_element != chars[char]:
+                        return False
+
+                else:
+                    stack.append(char)
+            return len(stack) == 0
+            
+          
